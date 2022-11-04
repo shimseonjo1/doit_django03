dbaa544f66c75234ac5e1d44b232e302a1464be7
shimseonjo1 <shimseonjo@naver.com>
2022-11-04 17:33:08 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ class PostList(ListView):
     model = Post
     ordering='-pk'
     # template_name = "blog/index.html"
-    
+
     def get_context_data(self,**kwargs):
         context = super(PostList,self).get_context_data()
         context['categories'] = Category.objects.all()
@@ -36,3 +36,26 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self,**kwargs):
+        context = super(PostDetail,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+def category_page(request,slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list':post_list,
+            'categories':Category.objects.all(),
+            'no_category_post_count':Post.objects.filter(category=None).count(),
+            'category':category
+        }
+    )
