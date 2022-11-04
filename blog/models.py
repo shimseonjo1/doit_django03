660713e054d17a4a95177f2767aa83d19626e3e5
shimseonjo1 <shimseonjo@naver.com>
2022-11-04 10:54:09 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.id}] {self.title} :: {self.author}'
